06bb0b0ae207a4cc9733162a98d77934b4457037
Stephen Olson <stephenolson@gmail.com>
2019-06-02 23:04:29 +0000

    Automatically backed up by Learn

--- db/migrate/20190602210642_create_movies.rb
+++ db/migrate/20190602210642_create_movies.rb
@@ -1,7 +1,7 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |m|
-      m.string :title
+      m.string :name
       m.integer :release_date
       m.string :director
       m.string :lead
